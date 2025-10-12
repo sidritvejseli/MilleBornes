e5ec7205c16517cbac456bf269cdfa67a8377e1a
sidritvejseli <sidrit2016@gmail.com>
2025-10-12 10:50:45 +0200

    fin tp2

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,5 +1,30 @@
 package jeu;
 
-public class Jeu {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import cartes.Carte;
+import cartes.JeuDeCartes;
+import utils.GestionCartes;
+
+public class Jeu {
+	Sabot sabot;
+	
+	public Jeu() {
+		JeuDeCartes jeuDeCartes = new JeuDeCartes();
+
+		List<Carte> listeCartes = new ArrayList<>();
+		Collections.addAll(listeCartes, jeuDeCartes.donnerCartes());
+
+		listeCartes = GestionCartes.melanger(listeCartes);
+		Carte[] tabMelange =  (Carte[]) listeCartes.toArray();
+		
+		sabot = new Sabot(tabMelange);
+		
+		
+		
+		
+	}
 }
